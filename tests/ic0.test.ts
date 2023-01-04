bb99e836eabfd3bb6036470b6b975195441cbd20
rvanasa <ryan.vandersmith@dfinity.org>
2023-01-04 11:28:13 -0700

    Add unit tests for 'mockCanister()'

--- tests/ic0.test.ts
+++
@@ -1,5 +0,0 @@
-describe('ic0', () => {
-    test('placeholder', () => {
-        expect(1).toEqual(1);
-    });
-});
---
+++ tests/mockCanister.test.ts
@@ -0,0 +1,29 @@
+import { mockCanister } from '../src/index';
+
+describe('mockCanister', () => {
+    test('basic example', async () => {
+        const mock = mockCanister({
+            async echo(x: number) {
+                return x;
+            },
+        });
+        expect(await mock.call('echo', 123)).toEqual(123);
+    });
+
+    test('fallback', async () => {
+        const mockEcho = mockCanister({
+            async echo(x: number) {
+                return x;
+            },
+        });
+        const mock = mockCanister(
+            {
+                async runMock() {
+                    return this.call('echo', 123);
+                },
+            },
+            mockEcho,
+        );
+        expect(await mock.call('runMock')).toEqual(123);
+    });
+});
